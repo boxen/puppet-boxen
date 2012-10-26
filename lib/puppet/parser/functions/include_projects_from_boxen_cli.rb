565946b8e45fe9fec8cc8bda4d6e7e6e886dc784
Yossef Mendelssohn <ymendel@pobox.com>
2012-10-26 15:26:00 -0400

    include projects from boxen CLI
    A puppet function! And its use in boxen::environment! Joy for everyone!

---
+++ lib/puppet/parser/functions/include_projects_from_boxen_cli.rb
@@ -0,0 +1,17 @@
+module Puppet::Parser::Functions
+  newfunction(:include_projects_from_boxen_cli) do |args|
+    Puppet::Parser::Functions.function('include')
+
+    cli_projects = Facter[:cli_boxen_projects].value
+    cli_projects.split(',').each do |project|
+      path = "#{Facter[:boxen_repodir].value}/modules/projects/manifests/#{project}.pp"
+
+      if File.exist?(path)
+        warn "Setting up '#{project}'. This can be made permanent by having 'include projects::#{project}' in your personal manifest."
+        function_include ["projects::#{project}"]
+      else
+        warn "Don't know anything about '#{project}'. Help out by defining it at '#{path}'."
+      end
+    end
+  end
+end
