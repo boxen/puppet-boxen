0a34764caeb4f471ed90c7b9d7596323729886d1
Will Farrington <wfarr@github.com>
2012-11-02 11:29:33 -0400

    don't error out if cli_boxen_projects fact is MIA

--- lib/puppet/parser/functions/include_projects_from_boxen_cli.rb
+++ lib/puppet/parser/functions/include_projects_from_boxen_cli.rb
@@ -2,16 +2,16 @@ module Puppet::Parser::Functions
   newfunction(:include_projects_from_boxen_cli) do |args|
     Puppet::Parser::Functions.function('include')
 
-    cli_projects = Facter[:cli_boxen_projects].value
+    if Facter[:cli_boxen_projects] && cli_projects = Facter[:cli_boxen_projects].value
+      cli_projects.split(',').each do |project|
+        path = "#{Facter[:boxen_repodir].value}/modules/projects/manifests/#{project}.pp"
 
-    cli_projects.split(',').each do |project|
-      path = "#{Facter[:boxen_repodir].value}/modules/projects/manifests/#{project}.pp"
-
-      if File.exist?(path)
-        warning "Setting up '#{project}'. This can be made permanent by having 'include projects::#{project}' in your personal manifest."
-        function_include ["projects::#{project}"]
-      else
-        warning "Don't know anything about '#{project}'. Help out by defining it at '#{path}'."
+        if File.exist?(path)
+          warning "Setting up '#{project}'. This can be made permanent by having 'include projects::#{project}' in your personal manifest."
+          function_include ["projects::#{project}"]
+        else
+          warning "Don't know anything about '#{project}'. Help out by defining it at '#{path}'."
+        end
       end
     end
   end
