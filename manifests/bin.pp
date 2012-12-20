c08848db754b3e9dce8304b3019470d987c18cc5
Will Farrington <wfarr@github.com>
2012-12-20 13:03:26 -0800

    fix dep

--- manifests/bin.pp
+++ manifests/bin.pp
@@ -4,6 +4,6 @@ class boxen::bin {
   file { "${boxen::config::home}/bin/boxen":
     ensure  => link,
     target  => "${::boxen_home}/repo/script/boxen",
-    require => Repository["${::boxen_home}/repo"]
+    require => File["${::boxen_home}/repo/.git"]
   }
 }
