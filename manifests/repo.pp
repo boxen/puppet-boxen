c3c8f306d5bd6ab7f3c50bb56e57dd440e0ff790
Will Farrington <wfarr@github.com>
2012-12-20 12:59:38 -0800

    first pass at fixing run from boxen-web

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,6 +1,13 @@
 class boxen::repo {
-  repository { "${::boxen_home}/repo":
-    ensure => present,
-    source => $boxen::config::reponame
+  $remote_add = "git remote add origin https://github.com/${boxen::config::reponame}"
+  $git_fetch = "git fetch -q origin"
+  $git_reset = "git reset --hard origin/master"
+
+  file { "${::boxen_home}/repo": ensure => directory }
+
+  exec { "clone ${::boxen_home}/repo":
+    command => "${remote_add} && ${git_fetch} && ${git_reset}",
+    creates => "${::boxen_home}/repo/.git",
+    cwd     => "${::boxen_home}/repo",
   }
 }
