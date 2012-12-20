aecb2c25835e1079fdbea07f95d629f4844fac58
Will Farrington <wfarr@github.com>
2012-12-20 13:16:42 -0800

    make sure to git init first

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,12 +1,13 @@
 class boxen::repo {
+  $git_init   = "git init"
   $remote_add = "git remote add origin https://github.com/${boxen::config::reponame}"
-  $git_fetch = "git fetch -q origin"
-  $git_reset = "git reset --hard origin/master"
+  $git_fetch  = "git fetch -q origin"
+  $git_reset  = "git reset --hard origin/master"
 
   file { "${::boxen_home}/repo": ensure => directory }
 
   exec { "clone ${::boxen_home}/repo":
-    command => "${remote_add} && ${git_fetch} && ${git_reset}",
+    command => "${git_init} && ${remote_add} && ${git_fetch} && ${git_reset}",
     creates => "${::boxen_home}/repo/.git",
     cwd     => "${::boxen_home}/repo",
   }
