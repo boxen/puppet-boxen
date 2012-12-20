942219101b7b94847f793907b36704145580c141
Will Farrington <wfarr@github.com>
2012-12-20 13:05:19 -0800

    fix deps

--- manifests/bin.pp
+++ manifests/bin.pp
@@ -4,6 +4,6 @@ class boxen::bin {
   file { "${boxen::config::home}/bin/boxen":
     ensure  => link,
     target  => "${::boxen_home}/repo/script/boxen",
-    require => File["${::boxen_home}/repo/.git"]
+    require => Exec["clone ${::boxen_home}/repo"]
   }
 }
