950b20bee6290f9a29c7934c18d187575de1a88c
Will Farrington <wfarr@github.com>
2013-02-14 18:39:37 -0800

    clean up

--- lib/facter/root_encrypted.rb
+++ lib/facter/root_encrypted.rb
@@ -1,13 +1,12 @@
-require "json"
 require "boxen/config"
 
-config   = Boxen::Config.load
-
-def root_encrypted?
-  system("/usr/sbin/diskutil coreStorage info / > /dev/null 2>&1")
-end
-
 Facter.add("root_encrypted") do
+  config = Boxen::Config.load
+
+  def root_encrypted?
+    system("/usr/sbin/diskutil coreStorage info / > /dev/null 2>&1")
+  end
+
   setcode do
     ENV['BOXEN_NO_FDE'] || !config.fde? || root_encrypted? ? 'yes' : 'no'
   end
