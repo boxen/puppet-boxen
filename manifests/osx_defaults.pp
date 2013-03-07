63b1aace5aa9648c79baa1c46f7ac5be2a2f5107
David Goodlad <david@goodlad.net>
2013-03-07 21:41:46 +1100

    Support the host/currentHost options to defaults
    `defaults` allows either `-currentHost` or `-host [hostname]` to be
    given as the first argument. This is required to set certain system-wide
    values on OSX, e.g. NSGlobalDomain com.apple.mouse.tapBehavior.

--- manifests/osx_defaults.pp
+++ manifests/osx_defaults.pp
@@ -1,5 +1,6 @@
 define boxen::osx_defaults(
   $ensure = 'present',
+  $host   = undef,
   $domain = undef,
   $key    = undef,
   $value  = undef,
@@ -7,26 +8,31 @@ define boxen::osx_defaults(
   $type   = undef,
 ) {
   $defaults_cmd = '/usr/bin/defaults'
+  case $host {
+    'currentHost': { $host_option = ' -currentHost' }
+    undef:         { $host_option = '' }
+    default:       { $host_option = " -host ${host}" }
+  }
 
   if $ensure == 'present' {
     if ($domain != undef) and ($key != undef) and ($value != undef) {
       if ($type != undef) {
-        $cmd = "${defaults_cmd} write ${domain} ${key} -${type} '${value}'"
+        $cmd = "${defaults_cmd}${host_option} write ${domain} ${key} -${type} '${value}'"
       } else {
-        $cmd = "${defaults_cmd} write ${domain} ${key} '${value}'"
+        $cmd = "${defaults_cmd}${host_option} write ${domain} ${key} '${value}'"
       }
-      exec { "osx_defaults write ${domain}:${key}=>${value}":
+      exec { "osx_defaults write ${host} ${domain}:${key}=>${value}":
         command => "${cmd}",
-        unless  => "${defaults_cmd} read ${domain} ${key} && (${defaults_cmd} read ${domain} ${key} | awk '{ exit \$0 != \"${value}\" }')",
+        unless  => "${defaults_cmd}${host_option} read ${domain} ${key} && (${defaults_cmd} read ${domain} ${key} | awk '{ exit \$0 != \"${value}\" }')",
         user    => $user
       }
     } else {
       warning('Cannot ensure present without domain, key, and value attributes')
     }
   } else {
-    exec { "osx_defaults delete ${domain}:${key}":
-      command => "${defaults_cmd} delete ${domain} ${key}",
-      onlyif  => "${defaults_cmd} read ${domain} | grep ${key}",
+    exec { "osx_defaults delete ${host} ${domain}:${key}":
+      command => "${defaults_cmd}${host_option} delete ${domain} ${key}",
+      onlyif  => "${defaults_cmd}${host_option} read ${domain} | grep ${key}",
       user    => $user
     }
   }
