46d17a7aad016ce1d904cded039b4a65e2158325
David Goodlad <david@goodlad.net>
2013-03-09 10:37:07 +1100

    Add simple specs around boxen::osx_defaults

---
+++ spec/defines/osx_defaults_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'boxen::osx_defaults' do
+  let(:title)  { 'example' }
+  let(:domain) { 'com.example' }
+  let(:key)    { 'testkey' }
+  let(:value)  { 'yes' }
+
+  let(:params) {
+    { :domain => domain,
+      :key    => key,
+      :value  => value,
+    }
+  }
+
+  it do
+    should contain_exec("osx_defaults write  #{domain}:#{key}=>#{value}")
+  end
+
+  context "currentHost" do
+    let(:host) { 'currentHost' }
+    let(:params) {
+      { :domain => domain,
+        :key    => key,
+        :value  => value,
+        :host   => host
+      }
+    }
+
+    it do
+      should contain_exec("osx_defaults write #{host} #{domain}:#{key}=>#{value}")
+    end
+  end
+end
