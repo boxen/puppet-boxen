3d822b0b50a859c72a4739c6a61d4ce4052c826e
drewzar <drew@drewzar.com>
2013-07-19 12:56:34 -0700

    refactor to include system

--- lib/facter/root_encrypted.rb
+++ lib/facter/root_encrypted.rb
@@ -4,8 +4,8 @@ Facter.add("root_encrypted") do
   config = Boxen::Config.load
 
   def root_encrypted?
-    is_active = `/usr/bin/fdesetup isactive ; echo $?`.chomp
-	  is_active == "0" || is_active == "2" ? true : false
+    system("/usr/bin/fdesetup isactive /")
+    [0, 2].include? $?
   end
 
   setcode do
