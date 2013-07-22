c545a6797c0714c0ab38d8d715376b8ff616a637
Will Farrington <wfarr@github.com>
2013-07-21 23:53:01 -0700

    Bring facts in-line with the boxen gem

--- lib/facter/boxen.rb
+++ lib/facter/boxen.rb
@@ -1,31 +1,40 @@
 require "json"
 require "boxen/config"
 
-config   = Boxen::Config.load
-facts    = {}
-factsdir = File.join config.homedir, "config", "facts"
+config      = Boxen::Config.load
+facts       = {}
+factsdir    = "#{config.homedir}/config/facts"
+dot_boxen   = "#{ENV['HOME']}/.boxen"
+user_config = "#{dot_boxen}/config.json"
 
-facts["github_login"]   = config.login
-facts["github_email"]   = config.email
-facts["github_name"]    = config.name
+facts["github_login"]  = config.login
+facts["github_email"]  = config.email
+facts["github_name"]   = config.name
+facts["github_token"]  = config.token
 
-facts["boxen_home"]     = config.homedir
-facts["boxen_srcdir"]   = config.srcdir
+facts["boxen_home"]    = config.homedir
+facts["boxen_srcdir"]  = config.srcdir
+facts["boxen_repodir"] = config.repodir
+facts["boxen_user"]    = config.user
+facts["luser"]         = config.user # this is goin' away
 
-if config.respond_to? :reponame
-  facts["boxen_reponame"] = config.reponame
+Dir["#{config.homedir}/config/facts/*.json"].each do |file|
+  facts.merge! JSON.parse File.read file
 end
 
-facts["luser"]          = config.user
+if File.directory?(dot_boxen) && File.file?(user_config)
+  facts.merge! JSON.parse(File.read(user_config))
+end
+
+if File.file?(dot_boxen)
+  warn "DEPRECATION: ~/.boxen is deprecated and will be removed in 2.0; use ~/.boxen/config.json instead!"
+  facts.merge! JSON.parse(File.read(dot_boxen))
+end
 
 if config.respond_to? :repotemplate
   facts["boxen_repo_url_template"] = config.repotemplate
 else
   facts["boxen_repo_url_template"] = "https://github.com/%s"
-end
-
-Dir["#{config.homedir}/config/facts/*.json"].each do |file|
-  facts.merge! JSON.parse File.read file
 end
 
 facts.each do |k, v|
