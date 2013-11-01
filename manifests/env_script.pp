9090a5a38d6c6befe576202ec49b011f86d83e6b
Will Farrington <wfarr@github.com>
2013-11-01 16:04:46 -0700

    add fallback to manual variable for env_script priority

--- manifests/env_script.pp
+++ manifests/env_script.pp
@@ -1,14 +1,3 @@
-# Public: install a boxen environment script
-# with the given priority.
-#
-# Usage:
-#
-#   boxen::env_script {
-#    'config':
-#      content  => template('boxen/config.sh.erb'),
-#      priority => 'highest';
-#   }
-
 define boxen::env_script(
   $ensure     = 'present',
   $scriptname = $name,
@@ -18,17 +7,18 @@ define boxen::env_script(
 ) {
 
   if $source == undef and $content == undef {
-    fail('One of source or content must not be undef!')
+    fail("One of source or content must not be undef!")
   }
 
   $real_priority = $priority ? {
-    'highest' => '10',
-    'higher'  => '30',
-    'high'    => '40',
-    'normal'  => '50',
-    'low'     => '60',
-    'lower'   => '70',
-    'lowest'  => '90',
+    'highest' => 10,
+    'higher'  => 30,
+    'high'    => 40,
+    'normal'  => 50,
+    'low'     => 60,
+    'lower'   => 70,
+    'lowest'  => 90,
+    default   => $priority,
   }
 
   include boxen::config
@@ -38,4 +28,5 @@ define boxen::env_script(
     source  => $source,
     content => $content,
   }
+
 }
