0de2b2ac1eda5ec39f020a3c8b125735b4aded68
David Goodlad <david@goodlad.net>
2014-02-01 15:24:02 -0500

    Set boxen::config variables via hiera data

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,21 +4,22 @@
 #
 #   include boxen::config
 
-class boxen::config {
-  $home              = $::boxen_home
-  $bindir            = "${home}/bin"
-  $cachedir          = "${home}/cache"
-  $configdir         = "${home}/config"
-  $datadir           = "${home}/data"
-  $envdir            = "${home}/env.d"
-  $homebrewdir       = "${home}/homebrew"
-  $logdir            = "${home}/log"
-  $repodir           = $::boxen_repodir
-  $reponame          = $::boxen_reponame
-  $socketdir         = "${datadir}/project-sockets"
-  $srcdir            = $::boxen_srcdir
-  $login             = $::github_login
-  $repo_url_template = $::boxen_repo_url_template
+class boxen::config (
+  $home,
+  $bindir,
+  $cachedir,
+  $configdir,
+  $datadir,
+  $envdir,
+  $homebrewdir,
+  $logdir,
+  $repodir,
+  $reponame,
+  $socketdir,
+  $srcdir,
+  $login,
+  $repo_url_template,
+) {
 
   file { [$home,
           $srcdir,
