f071cdfc26e5ec92ecdef323891a871ff1edb68c
David Goodlad <david@goodlad.net>
2014-02-01 16:35:50 -0500

    Satisfy the linter

--- manifests/osx_defaults.pp
+++ manifests/osx_defaults.pp
@@ -37,8 +37,8 @@ define boxen::osx_defaults(
       }
 
       $write_cmd = $type_ ? {
-        undef   => shellquote($default_cmds, 'write', $domain, $key, "${value}"),
-        default => shellquote($default_cmds, 'write', $domain, $key, "-${type_}", "${value}")
+        undef   => shellquote($default_cmds, 'write', $domain, $key, strip("${value} ")),
+        default => shellquote($default_cmds, 'write', $domain, $key, "-${type_}", strip("${value} "))
       }
 
       $read_cmd = shellquote($default_cmds, 'read', $domain, $key)
