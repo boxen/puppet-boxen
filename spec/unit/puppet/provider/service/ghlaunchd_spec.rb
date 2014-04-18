21a10dcf0a55d59ea38a04a6e5e0a513afefd9f8
Soeren Krings <soeren@jimdo.com>
2014-04-18 10:40:26 +0200

    Fixup tests for ghlaunchd by switch from mocha to rspec-mocks api
     * Also had an issue with stubbing boxen related facts

--- spec/unit/puppet/provider/service/ghlaunchd_spec.rb
+++ spec/unit/puppet/provider/service/ghlaunchd_spec.rb
@@ -6,57 +6,61 @@ describe Puppet::Type.type(:service).provider(:ghlaunchd) do
     resource = Puppet::Type.type(:service).hash2resource({:name => 'some.vendor.service'})
     described_class.new(resource)
   }
+  let(:facts) {{
+    :operatingsystem => 'darwin'
+  }}
+
+  Facter.add('boxen_user') { setcode { 'some_user' } }
 
   describe "start" do
 
     context "with some service plist" do
 
       it "should load a launch agent with sudo" do
-        Dir.stubs(:glob).returns(['/Library/LaunchAgents/some.vendor.service.plist'])        
-        Facter.stubs(:fact).with(:boxen_user).returns(stub(:value => 'some_user'))
+        Dir.stub(:glob).and_return(['/Library/LaunchAgents/some.vendor.service.plist'])
 
-        subject.stubs(:plutil).returns('{}')
-        subject.stubs(:command).with(:launchctl).returns('/bin/launchctl')
-        subject.expects(:sudo).with('-u', 'some_user', '/bin/launchctl', :load, '-w', '/Library/LaunchAgents/some.vendor.service.plist')
-        subject.expects(:sudo).with('-u', 'some_user', '/bin/launchctl', :start, 'some.vendor.service')
+        subject.stub(:plutil).and_return('{}')
+        subject.stub(:command).with(:launchctl).and_return('/bin/launchctl')
+        subject.should_receive(:sudo).with('-u', 'some_user', '/bin/launchctl', :load, '-w', '/Library/LaunchAgents/some.vendor.service.plist')
+        subject.should_receive(:sudo).with('-u', 'some_user', '/bin/launchctl', :start, 'some.vendor.service')
         subject.start
       end
 
       it "should load but not start a inetd compatible launch agent" do
-        Dir.stubs(:glob).returns(['/Library/LaunchAgents/some.vendor.service.plist'])        
-        Facter.stubs(:fact).with(:boxen_user).returns(stub(:value => 'some_user'))
+        Dir.stub(:glob).and_return(['/Library/LaunchAgents/some.vendor.service.plist'])
 
-        subject.stubs(:plutil).returns('{}')
-        subject.stubs(:command).with(:launchctl).returns('/bin/launchctl')
-        subject.stubs(:config).returns({'inetdCompatibility' => {}})
-        subject.expects(:sudo).with('-u', 'some_user', '/bin/launchctl', :load, '-w', '/Library/LaunchAgents/some.vendor.service.plist')
-        subject.expects(:sudo).with('-u', 'some_user', '/bin/launchctl', :start, 'some.vendor.service').never()
+        subject.stub(:plutil).and_return('{}')
+        subject.stub(:command).with(:launchctl).and_return('/bin/launchctl')
+        subject.stub(:config).and_return({'inetdCompatibility' => {}})
+        subject.should_receive(:sudo).with('-u', 'some_user', '/bin/launchctl', :load, '-w', '/Library/LaunchAgents/some.vendor.service.plist')
+        subject.should_receive(:sudo).with('-u', 'some_user', '/bin/launchctl', :start, 'some.vendor.service').never
         subject.start
-      end  
+      end
 
       it "should load a launch deamon without sudo" do
-        Dir.stubs(:glob).returns(['/Library/LaunchDeamons/some.vendor.service.plist'])
+        Dir.stub(:glob).and_return(['/Library/LaunchDeamons/some.vendor.service.plist'])
 
-        subject.stubs(:plutil).returns('{}')
-        subject.expects(:launchctl).with(:load, '-w', '/Library/LaunchDeamons/some.vendor.service.plist')
-        subject.expects(:launchctl).with(:start, 'some.vendor.service')
+        subject.stub(:plutil).and_return('{}')
+        subject.should_receive(:launchctl).with(:load, '-w', '/Library/LaunchDeamons/some.vendor.service.plist')
+        subject.should_receive(:launchctl).with(:start, 'some.vendor.service')
         subject.start
       end
     end
 
     context "without some service plist" do
-      Dir.stubs(:glob).returns([])
 
       it "should not load a launch agent" do
-        Facter.stubs(:fact).with(:boxen_user).returns(stub(:value => 'some_user'))
+        Dir.stub(:glob).and_return([])
 
-        subject.stubs(:command).with(:launchctl).returns('/bin/launchctl')
-        subject.expects(:sudo).with('-u', 'some_user', '/bin/launchctl', :load).never()
+        subject.stub(:command).with(:launchctl).and_return('/bin/launchctl')
+        subject.should_receive(:sudo).with('-u', 'some_user', '/bin/launchctl', :load).never
         subject.start.should be_false
       end
 
       it "should not load a launch deamon" do
-        subject.expects(:launchctl).with(:load).never()
+        Dir.stub(:glob).and_return([])
+
+        subject.should_receive(:launchctl).with(:load).never
         subject.start.should be_false
       end
     end
@@ -65,39 +69,39 @@ describe Puppet::Type.type(:service).provider(:ghlaunchd) do
   describe "stop" do
 
     context "with some service plist" do
-    
-      it "should unload a launch agent" do
-        Dir.stubs(:glob).returns(['/Library/LaunchAgents/some.vendor.service.plist'])
-        Facter.stubs(:fact).with(:boxen_user).returns(stub(:value => 'some_user'))
 
-        subject.stubs(:plutil).returns('{}')
-        subject.stubs(:command).with(:launchctl).returns('/bin/launchctl')
-        subject.expects(:sudo).with('-u', 'some_user', '/bin/launchctl', :unload, '-w','/Library/LaunchAgents/some.vendor.service.plist')
+      it "should unload a launch agent" do
+        Dir.stub(:glob).and_return(['/Library/LaunchAgents/some.vendor.service.plist'])
+
+        subject.stub(:plutil).and_return('{}')
+        subject.stub(:command).with(:launchctl).and_return('/bin/launchctl')
+        subject.should_receive(:sudo).with('-u', 'some_user', '/bin/launchctl', :unload, '-w', '/Library/LaunchAgents/some.vendor.service.plist')
         subject.stop
       end
 
       it "should unload a launch deamon" do
-        Dir.stubs(:glob).returns(['/Library/LaunchDeamons/some.vendor.service.plist'])        
+        Dir.stub(:glob).and_return(['/Library/LaunchDeamons/some.vendor.service.plist'])
 
-        subject.stubs(:plutil).returns('{}')
-        subject.expects(:launchctl).with(:unload, '-w', '/Library/LaunchDeamons/some.vendor.service.plist')
+        subject.stub(:plutil).and_return('{}')
+        subject.should_receive(:launchctl).with(:unload, '-w', '/Library/LaunchDeamons/some.vendor.service.plist')
         subject.stop
       end
     end
 
     context "without some service plist" do
-      Dir.stubs(:glob).returns([])
 
       it "should not unload a launch agent" do
-        Facter.stubs(:fact).with(:boxen_user).returns(stub(:value => 'some_user'))
+        Dir.stub(:glob).and_return([])
 
-        subject.stubs(:command).with(:launchctl).returns('/bin/launchctl')
-        subject.expects(:sudo).with('-u', 'some_user', '/bin/launchctl', :load).never()
+        subject.stub(:command).with(:launchctl).and_return('/bin/launchctl')
+        subject.should_receive(:sudo).with('-u', 'some_user', '/bin/launchctl', :load).never
         subject.stop.should be_false
       end
 
       it "should not unload a launch deamon" do
-        subject.expects(:launchctl).with(:unload).never()
+        Dir.stub(:glob).and_return([])
+
+        subject.should_receive(:launchctl).with(:unload).never
         subject.stop.should be_false
       end
     end
