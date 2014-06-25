a37932e1962c9184b565348caa462e8e7793b757
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-06-25 17:46:20 -0300

    Reuse github token to avoid API limit errors

--- lib/puppet/parser/functions/boxen_repos.rb
+++ lib/puppet/parser/functions/boxen_repos.rb
@@ -4,7 +4,15 @@ module Puppet::Parser::Functions
   newfunction(:boxen_repos, :type => :rvalue) do |args|
     repos_per_page = 100
 
-    repo_count = Octokit.org(:boxen).public_repos
+    token = Facter.value('github_token')
+
+    if token
+      client = Octokit::Client.new(:access_token => token)
+    else
+      client = Octokit::Client.new
+    end
+
+    repo_count = client.org(:boxen).public_repos
     pages = (repo_count / repos_per_page.to_f).round
     repos = []
 
@@ -12,7 +20,7 @@ module Puppet::Parser::Functions
 
     pages.times do |page|
       opts[:page] = page + 1
-      repos.concat Octokit.org_repos(:boxen, opts).map(&:name)
+      repos.concat client.org_repos(:boxen, opts).map(&:name)
     end
 
     repos
