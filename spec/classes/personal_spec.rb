eee3822cea1f5a110ff9e508e98eced30f078499
Tim Taylor <tim@tool-man.org>
2015-01-12 23:04:49 -0800

    specs for boxen::personal::dotfiles

--- spec/classes/personal_spec.rb
+++ spec/classes/personal_spec.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
 
 describe 'boxen::personal' do
-  
+
+  let(:username) { 'testuser' }
+  let(:facts) { {
+    :boxen_home => '/opt/boxen',
+    :boxen_repodir => 'spec/fixtures',
+    :github_login => username
+  } }
+
   context "username with dash" do
-    let(:facts) do
-      {
-        :boxen_home => '/opt/boxen',
-        :boxen_repodir => 'spec/fixtures',
-        :github_login => 'some-username',
-      }
-    end
+    let(:username) { 'some-username' }
 
     it { should include_class('boxen::config')}
     it { should include_class('people::some_username')}
   end
+
+  context 'dotfiles omitted' do
+    it { should_not include_class('dotfiles')}
+  end
+
+  context 'dotfiles specified' do
+    let(:params) { { :dotfiles => ['somethingrc'] } }
+
+    it { should contain_dotfiles__symlink('somethingrc')}
+  end
+
 end
