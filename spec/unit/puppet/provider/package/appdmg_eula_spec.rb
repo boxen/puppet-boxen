34b5bad4fde81f1b54ffb04daecf8c15a306f26f
n0ts <naoya.n@gmail.com>
2015-10-21 22:49:59 +0900

    Fix test

--- spec/unit/puppet/provider/package/appdmg_eula_spec.rb
+++ spec/unit/puppet/provider/package/appdmg_eula_spec.rb
@@ -31,7 +31,7 @@ describe Puppet::Type.type(:package).provider(:appdmg_eula) do
       it "should call tmpdir and use the returned directory" do
         Dir.should_receive(:mktmpdir).and_return tmpdir
         Dir.stub(:entries).and_return ["foo.app"]
-        described_class.should_receive(:curl).with('-o', "#{tmpdir}/foo", '-C', '-', '-k', '-L', '-s', '--url', source)
+        described_class.should_receive(:curl).with('-o', "#{tmpdir}/foo", '-C', '-', '-L', '-s', '--url', source)
         described_class.should_receive(:hdiutil).with('convert', '/tmp/foo', '-format', 'UDTO', '-o', '/tmp/good123/appdmg_eula')
         described_class.should_receive(:hdiutil).with('attach', '-plist', '-nobrowse', '-readonly', '-noverify', '-noautoopen', '-mountrandom', '/tmp', "#{tmpdir}/appdmg_eula.cdr").and_return(fake_hdiutil_plist)
         described_class.should_receive(:installapp)
